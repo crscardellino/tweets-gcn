91592a22abe2ca6b0b8e027072d0dca456b80068
Cristian Cardellino <crscardellino@gmail.com>
2019-12-02 20:16:05 -0300

    Added a couple of templates for running experiments

---
+++ base_exp_features_supervised.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/evn bash
+
+set -e
+
+(for c in corrected original
+do
+    BASE_NAME="semeval.abortion.data.${c}.supervised"
+    python data_processing.py \
+        --char-ngrams 2 3 4 \
+        --graph-hashtags \
+        --graph-mentions \
+        --graph-ngrams 3 4 5 \
+        --ignore-hashtag semst \
+        --min-docs 2 \
+        --remove-links \
+        --remove-mentions \
+        --remove-numeric \
+        --reduce-tweet-word-len \
+        --tweet-lowercase \
+        ./data/$BASE_NAME/$BASE_NAME.csv.gz \
+        ./data/$BASE_NAME/$BASE_NAME &
+done
+wait)
